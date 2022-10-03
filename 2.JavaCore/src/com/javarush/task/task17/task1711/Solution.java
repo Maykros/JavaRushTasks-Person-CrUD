79c6e14797c767e0306510c614d07319b836ad68
Maykros <dan.bajckov@yandex.ru>
2022-10-03 15:39:55 +0300

    create operation

--- 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task17.task1711;
 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -23,14 +24,15 @@ public class Solution {
         SimpleDateFormat formatterSlash = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
         SimpleDateFormat formatterHyph = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
         Person person = null;
-        int operationCUCount = (args.length - 1) / 3;
-        int operationRDCount = args.length - 1;
+        int createCount = (args.length - 1) / 3;
+        int updateCount = (args.length - 1) / 3;
+        int readCount = args.length - 1;
 
         switch (args[0]) {
             case "-c":
-                for (int i = 0, j = 1; i < operationCUCount; i++, j = j + 3) {
+                for (int i = 0, j = 1; i < createCount; i++, j = j + 3) {
                     if (args[j+1].equals("м")) {
-                        allPeople.add(Person.createMale(args[j], formatterSlash.parse(args[j+2])));
+                        allPeople.add(Person.createMale(args[j], formatterSlash.parse(args[j + 2])));
                         System.out.println(i);
                     }
                     else {
@@ -40,13 +42,24 @@ public class Solution {
                 }
                 break;
             case "-u":
-
+                for (int i = 0, j = 1; i < updateCount; i++, j = j + 4) {
+                    person = allPeople.get(Integer.parseInt(args[j]));
+                    person.setName(args[j + 1]);
+                    person.setSex(args[j + 2].equals("м") ? Sex.MALE : Sex.FEMALE);
+                    person.setBirthDate(formatterSlash.parse(args[j + 3]));
+                }
                 break;
             case "-d":
-
+                for (int i = 0, j = 1; i < updateCount; i++, j = j + 2) {
+                    allPeople.remove(Integer.parseInt(args[j]));
+                }
                 break;
             case "-i":
-
+                for (int i = 0, j = 1; i < readCount; i++, j++) {
+                    person = allPeople.get(Integer.parseInt(args[j]));
+                    String sex = person.getSex().equals(Sex.MALE) ? "м" : "ж";
+                    System.out.println(person.getName() + " " + sex + " " + formatterHyph.format(person.getBirthDate()));
+                }
                 break;
         }
     }
